d423ffcc02e4e6c0ac6812a4ac9fa8f3c33de8a9
CP Müller <support@webdesigndenhaag.net>
2018-03-18 21:07:57 +0100

    Main plugin file

---
+++ wp-startup.php
@@ -0,0 +1,10 @@
+/*
+Plugin Name: WP Startup
+Plugin URI:  https://github.com/webbouwer/wp-startup
+Description: Do a lot more with a basic WP install
+Version:     1.0
+Author:      Webbouwer
+Author URI:  http://webdesigndenhaag.net
+License:     Copyright 2018
+License URI: http://webdesigndenhaag.net
+*/
