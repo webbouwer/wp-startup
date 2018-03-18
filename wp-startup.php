408ab352de0a30b6231a5a940f38e1273bf081f8
CP Müller <support@webdesigndenhaag.net>
2018-03-18 21:55:17 +0100

    Update wp-startup.php

--- wp-startup.php
+++ wp-startup.php
@@ -1,3 +1,4 @@
+<?php
 /*
 Plugin Name: WP Startup
 Plugin URI:  https://github.com/webbouwer/wp-startup
@@ -5,6 +6,18 @@ Description: Do a lot more with a basic WP install
 Version:     1.0
 Author:      Webbouwer
 Author URI:  http://webdesigndenhaag.net
-License:     Copyright 2018
+License:     © Oddsized All rights reserved
 License URI: http://webdesigndenhaag.net
 */
+
+require_once __DIR__ . '/src/autoloader.php';
+
+// Register the autoloader
+WP_Startup_Autoloader::register( 'WP_Startup', trailingslashit( plugin_dir_path( __FILE__ ) ) . '/src/' );
+// Runs this plugin after all plugins are loaded.
+add_action( 'plugins_loaded', function() {
+	$GLOBALS['wp_start'] = new WP_Startup_Plugin();
+	$GLOBALS['wp_start']->run( __FILE__ );
+});
+
+?>
