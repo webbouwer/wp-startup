f1d90473af8f9d511d89340aeb5f61c93668bcd9
CP Müller <support@webdesigndenhaag.net>
2018-03-20 11:42:45 +0000

    Update wp-startup.php

--- wp-startup.php
+++ wp-startup.php
@@ -11,17 +11,6 @@ License:     © Oddsized All rights reserved
 License URI: http://webdesigndenhaag.net
 */
 
-/**
-
-Features:
-
-- link manager (wp default hidden)
-- theme basic background-image
-- theme panorama image size 1800, 640,
-- widget text allow php
-- order categories in metabox
-
-*/
 
 /**
  * Verify code usage in WP
@@ -29,6 +18,7 @@ Features:
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
 
 /**
  * Load textdomain languages  
@@ -41,45 +31,54 @@ function wan_load_textdomain() {
 
 
 
-
-
-
 /**
- * WP Startup Admin menu
- * http://tekina.info/add-menus-submenus-wordpress-admin-panel/
- * https://developer.wordpress.org/resource/dashicons/#flag
+ * include settings.php
  */
-add_action( 'admin_menu', 'my_plugin_menu' );
-function my_plugin_menu() {
-    add_menu_page( 'My option page', 'WP Startup', 'manage_options', 'my-fist-slug', 'wp_startup_options_settings', 'dashicons-editor-kitchensink');
-    add_submenu_page('my-fist-slug', 'General Settings', 'Settings', 'manage_options', 'my-fist-slug', 'wp_startup_options_settings');
-    add_submenu_page('my-fist-slug', 'Admin options', 'Options', 'manage_options', 'my-second-slug', 'wp_startup_options_options');
-}
-/**
- * Admin menu page 1
- */
-function wp_startup_options_settings() {
-    echo 'WP Startup Plugin Settings';
-}
-/**
- * Admin menu page 2
- */
-function wp_startup_options_options() {
-    echo 'WP Startup Plugin Options';
-}
-
-
-
-
-
+require_once( 'settings.php' );
 
 
 
 /**
- * Link Manager
+ * Components
  * https://core.trac.wordpress.org/ticket/21307
  */
-add_filter( 'pre_option_link_manager_enabled', '__return_true' );
+function wpstartup_components_global() {
+
+	// Link Manager
+	if( get_option( 'ws_linkmanager_option' ) != '' && get_option( 'ws_linkmanager_option' ) == true ){
+		add_filter( 'pre_option_link_manager_enabled', '__return_true' );
+	}
+	// Keep category select list in hiÎarchy
+	// source http://wordpress.stackexchange.com/questions/61922/add-post-screen-keep-category-structure
+	if( get_option( 'ws_categoryhierarchy_option' ) != '' && get_option( 'ws_categoryhierarchy_option' ) == true ){
+		add_filter( 'widget_text', 'do_shortcode' );
+		add_filter( 'wp_terms_checklist_args', 'imagazine_wp_terms_checklist_args', 1, 2 );
+		function imagazine_wp_terms_checklist_args( $args, $post_id ) {
+			 $args[ 'checked_ontop' ] = false;
+			 return $args;
+		}
+	}	
+	// Text widget shortcodes 
+	if( get_option( 'ws_shortcodesintextwidget_option' ) != '' && get_option( 'ws_shortcodesintextwidget_option' ) == true ){
+		add_filter( 'widget_text', 'do_shortcode' );
+	}
+	// Text widget php
+	if( get_option( 'ws_phpintextwidget_option' ) != '' && get_option( 'ws_phpintextwidget_option' ) == true ){
+		
+		// Execute PHP in the default text-widget
+		add_filter('widget_text','php_execute',100);
+		function php_execute($html){
+			if(strpos($html,"<"."?php")!==false){ ob_start(); eval("?".">".$html);
+				$html=ob_get_contents();
+				ob_end_clean();
+			}
+			return $html;
+		}
+	}
+	
+}
+// on startup
+wpstartup_components_global();
 
 
 
@@ -89,31 +88,24 @@ add_filter( 'pre_option_link_manager_enabled', '__return_true' );
 /**
  * Register Theme (default) Support
  */
-function wpstartup_theme_global() {
-	add_image_size( 'panorama', 1800, 640, array( 'center', 'center' ) );
-	add_theme_support( 'custom-background' );
-}
 add_action( 'after_setup_theme', 'wpstartup_theme_global' ); 
+function wpstartup_theme_global() {
+	//add_image_size( 'panorama', 1800, 640, array( 'center', 'center' ) );
+	if( get_option( 'ws_themebgimage_option' ) != '' && get_option( 'ws_themebgimage_option' ) == true ){
+		add_theme_support( 'custom-background' );
+	}
 
-/** Libraries 
- *include webicon
- */
-function imagazine_load_webicons(){
-	wp_enqueue_script('jquery-webicon', '//cdn.rawgit.com/icons8/bower-webicon/v0.10.7/jquery-webicon.min.js');
 }
-add_action( 'wp_print_scripts', 'imagazine_load_webicons' );
-
 
 /** 
  * Add custom css
  */
 add_action( 'wp_head', 'ws_load_custom_css', 9999 );
 function ws_load_custom_css() {
-	
-	$csscode = 'h1.site-title a{ color:blue !important; }';
-	echo '<style>'.$csscode.'</style>';
-	
-	
+	if( get_option( 'ws_custom_css' ) != '' && get_option( 'ws_custom_css' ) != 1){
+		$csscode = get_option( 'ws_custom_css' );
+		echo '<style>'.$csscode.'</style>';
+	} 
 }
 
 /** 
@@ -121,136 +113,76 @@ function ws_load_custom_css() {
  */
 add_action( 'wp_head', 'ws_load_custom_js', 9998 );
 function ws_load_custom_js() {
-	
-	$jscode = 'alert("WP Startup activated!");';
-	echo '<script>'.$jscode.'</script>';
-	
-	
+	if( get_option( 'ws_custom_js' ) != '' && get_option( 'ws_custom_js' ) != 1){
+		$jscode = get_option( 'ws_custom_js' );
+		echo '<script>'.$jscode.'</script>';
+	} 	
 }
 
 
 
-/** Customized WP elements */
+
+/** Performance tweaks */
 
 /**
- * shortcodes in text widgets.
+ * control (remove) gravatar
  */
-add_filter( 'widget_text', 'do_shortcode' );
-
-/**
- * Execute PHP in the default text-widget
- */
-add_filter('widget_text','php_execute',100);
-function php_execute($html){
-	if(strpos($html,"<"."?php")!==false){ ob_start(); eval("?".">".$html);
-		$html=ob_get_contents();
-		ob_end_clean();
+function bp_remove_gravatar ($image, $params, $item_id, $avatar_dir, $css_id, $html_width, $html_height, $avatar_folder_url, $avatar_folder_dir) {
+	$default = home_url().'/wp-includes/images/smilies/icon_cool.gif'; // get_stylesheet_directory_uri() .'/images/avatar.png';
+	if( $image && strpos( $image, "gravatar.com" ) ){
+		return '<img src="' . $default . '" alt="avatar" class="avatar" ' . $html_width . $html_height . ' />';
+	} else {
+		return $image;
 	}
-	return $html;
 }
-/**
- * Keep category select list in hiÎarchy
- * source http://wordpress.stackexchange.com/questions/61922/add-post-screen-keep-category-structure
+function remove_gravatar ($avatar, $id_or_email, $size, $default, $alt) {
+	$default = home_url().'/wp-includes/images/smilies/icon_cool.gif'; // get_stylesheet_directory_uri() .'/images/avatar.png';
+	return "<img alt='{$alt}' src='{$default}' class='avatar avatar-{$size} photo avatar-default' height='{$size}' width='{$size}' />";
+}
+function bp_remove_signup_gravatar ($image) {
+	$default = home_url().'/wp-includes/images/smilies/icon_cool.gif'; //get_stylesheet_directory_uri() .'/images/avatar.png';
+	if( $image && strpos( $image, "gravatar.com" ) ){
+		return '<img src="' . $default . '" alt="avatar" class="avatar" width="60" height="60" />';
+	} else {
+		return $image;
+	}
+}
+
+if( get_option( 'ws_removegravatar_option' ) != '' && get_option( 'ws_removegravatar_option' ) == true ){
+	add_filter('bp_core_fetch_avatar', 'bp_remove_gravatar', 1, 9 );
+	add_filter('get_avatar', 'remove_gravatar', 1, 5);
+	add_filter('bp_get_signup_avatar', 'bp_remove_signup_gravatar', 1, 1 );
+}
+
+
+
+/** Remove Emoji junk by Christine Cooper
+ * Found on http://wordpress.stackexchange.com/questions/185577/disable-emojicons-introduced-with-wp-4-2
  */
-add_filter( 'wp_terms_checklist_args', 'imagazine_wp_terms_checklist_args', 1, 2 );
-function imagazine_wp_terms_checklist_args( $args, $post_id ) {
-	 $args[ 'checked_ontop' ] = false;
-	 return $args;
+function disable_wp_emojicons() {
+  // all actions related to emojis
+  remove_action( 'admin_print_styles', 'print_emoji_styles' );
+  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+  remove_action( 'wp_print_styles', 'print_emoji_styles' );
+  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+  add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' ); // filter to remove TinyMCE emojis
+}
+function disable_emojicons_tinymce( $plugins ) {
+  if ( is_array( $plugins ) ) {
+    return array_diff( $plugins, array( 'wpemoji' ) );
+  } else {
+    return array();
+  }
+}
+
+
+if( get_option( 'ws_removeemojicons_option' ) != '' && get_option( 'ws_removeemojicons_option' ) == true ){
+	add_action( 'init', 'disable_wp_emojicons' );
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-/** include googlefonts
-  
-function google_fonts() {
-	$query_args = array(
-		'family' => get_theme_mod("imagazine_global_styles_mainfont", "Lato|Martel"),
-		'subset' => get_theme_mod("imagazine_global_styles_subsetfont", "latin,latin-ext"),
-	);
-	wp_enqueue_style( 'google_fonts', add_query_arg( $query_args, "//fonts.googleapis.com/css" ), array(), null );
-	// wp_register_style
-}
-add_action('wp_enqueue_scripts', 'google_fonts');
-*/
-
-
-
-
-
-
-
-/*
- * Load code
- *
-
- 
-add_action('admin_enqueue_scripts', 'ln_reg_css_and_js');
-
-function ln_reg_css_and_js($hook)
-{
-
-    $current_screen = get_current_screen();
-
-    if ( strpos($current_screen->base, 'my-fist-slug') === false) {
-        return;
-    } else {
-
-        wp_enqueue_style('boot_css', plugins_url('inc/bootstrap.css',__FILE__ ));
-        wp_enqueue_script('boot_js', plugins_url('inc/bootstrap.js',__FILE__ ));
-        wp_enqueue_script('ln_script', plugins_url('inc/main_script.js', __FILE__), ['jquery'], false, true);
-        }
-    }
-
- */
- 
- 
- 
-
-/* language 
-https://premium.wpmudev.org/blog/translating-wordpress-plugins/
-https://premium.wpmudev.org/blog/how-to-translate-a-wordpress-plugin/
-
-https://wpcentral.io/internationalization/
-lang/*.mo
-
-msgid ""
-msgstr ""
-"Project-Id-Version: WP Startup 1.0.0\n"
-"Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2018-04-20 13:09+0100\n"
-"PO-Revision-Date: 2018-03-20 13:09+0100\n"
-"Last-Translator: Webbouwer <support@webdesigndenhaag.net>\n"
-"Language-Team: Webbouwer <support@webdesigndenhaag.net>\n"
-"Language: English \n"
-"MIME-Version: 1.0\n"
-"Content-Type: text/plain; charset=UTF-8\n"
-"Content-Transfer-Encoding: 8bit\n"
-"X-Poedit-KeywordsList: __;_e\n"
-"X-Poedit-Basepath: .\n"
-"X-Poedit-SearchPath-0: ..\n"
-
-#: ../wp-startup.php:20
-msgid "You are awesome"
-msgstr ""
-
-#: ../wp-startup.php:21
-msgid "This website is boss"
-msgstr ""
-
-#: ../wp-startup.php:22
-msgid "You look great today"
-msgstr ""
-
-*/
 ?>
