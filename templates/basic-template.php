daa795330e01382e861be27d11c89be9b8bfb503
webbouwer <support@webdesigndenhaag.net>
2018-03-25 23:10:22 +0200

    Add default theme functions

--- templates/basic-template.php
+++ templates/basic-template.php
@@ -3,5 +3,13 @@
  * Template name: Basic Template
  * Description: A minimal (blank) template to make anything you like.
  */
+
+get_header();
+
 echo 'This is a basic template file where the coder should make his Wordpress knowledge worth to notice';
+
+get_sidebar();
+
+get_footer();
+
 ?>
